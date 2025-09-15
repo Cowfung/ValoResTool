d472824d0b30116bcec98ccc9abd3340d1c5818c
Cowfung <cuongphamdinh19@gmail.com>
2025-09-15 10:24:28 +0700

    update 15-9 for laptop

--- Program.cs
+++ Program.cs
@@ -11,12 +11,7 @@ Console.WriteLine("  CÔNG CỤ CHỈNH MÀN HÌNH VALORANT 4:3");
 Console.WriteLine("==========================================");
 Console.WriteLine();
 
-bool didChange = ScalingService.SetScalingFullScreen();
-bool didFullScreen = ScalingService.SetScalingConfig();
-if (didChange || didFullScreen)
-{
-    GpuService.ResetGpu();
-}
+
 // 🔧 Trích xuất file từ resource
 string tempDir = Path.Combine(Path.GetTempPath(), "CowfungValoTool");
 Directory.CreateDirectory(tempDir);
